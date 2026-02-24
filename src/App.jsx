413804d1a8df95651372e5a92f3488b7afc082f3
Deep <diptiprasadsarangi44@gmail.com>
2026-02-24 17:40:32 +0530

    frontend folder structure added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='bg-slate-800 text-yellow-100'>HomePage</h1>
+    </>
+  )
+}
+
+export default App
